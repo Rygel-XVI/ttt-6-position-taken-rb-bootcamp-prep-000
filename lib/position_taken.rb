24f441f1e7dd5533c7a57aeb6059c2a037d44c4e
Annette Michaels <micha232@umn.edu>
2017-11-02 08:42:25 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   spot = board[index]
-  if (spot == " ")
-    return false
+  print board[index]
+#  if (spot == " ")
+#    return false
 end
